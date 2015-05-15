5ce2157c6d7a333f2a1739573bf59b81249066ce
RafaPinzon <rafapinzon_@hotmail.com>
2015-05-14 20:29:41 -0500

    Cambiado a entrega jueves

--- cliente.py
+++ cliente.py
@@ -3,11 +3,13 @@ __author__ = 'noescobar'
 import xmlrpclib
 import os
 
-clear = lambda: os.system('clear')
+clear = lambda: os.system('cls')
 
-proxy = xmlrpclib.ServerProxy("http://192.168.56.1:8000/")
+proxy = xmlrpclib.ServerProxy("http://192.168.9.32:8000/")
 
 result = proxy.serverInfo()
+
+ingresado = False
 
 print("Bienvenido al cliente\n\n")
 
@@ -16,96 +18,40 @@ print("Bienvenido al cliente\n\n")
 #  print(proxy.hello(nombre))
 
 while True:
-    print proxy.retornarMensajes
-    print("1. Registrarse")
-    print("2. Ingresar")
-    option = input('ingrese la opcion a elegir: ')
-
-    if(option == 1 ):
-        clear()
-        nombre = raw_input("Ingrese su nombre : ")
-        while(True):
+    print str(proxy.retornarMensajes())
+    if not ingresado:
+        print("1. Registrarse")
+        print("2. Ingresar")
+        option = input('ingrese la opcion a elegir: ')
+    
+        if(option == 1 ):
+            clear()
+            nombre = raw_input("Ingrese su nombre : ")
+            while(True):
+                password = raw_input("Ingrese su password : ")
+                confPass = raw_input("Confirme su password : ")
+                if(password == confPass):
+                    break
+            userInfo = [nombre,password]
+            print(str(proxy.registro(userInfo)))
+            print("\n\n")
+        elif(option == 2 ):
+            clear()
+            nombre = raw_input("Ingrese su nombre : ")
             password = raw_input("Ingrese su password : ")
-            confPass = raw_input("Confirme su password : ")
-            if(password == confPass):
-                break
-        userInfo = [nombre,password]
-        print(str(proxy.registro(userInfo)))
-        print("\n\n")
-    elif(option == 2 ):
-        clear()
-        nombre = raw_input("Ingrese su nombre : ")
-        password = raw_input("Ingrese su password : ")
-        userInfo = [nombre,password]
-        ingresado = proxy.ingresar(userInfo)
-        print("\n\n")
-        print(ingresado)
-        if(ingresado):
-            print("1. sumar dos numeros")
-            print("2. Restar dos numeros")
-            print("3. multiplicar dos numeros")
-            print("4. Dividir dos numeros")
-            print("5. Todas las operaciones anteriores")
-            print("6. Direccion ip y puerto del servidor")
-            print("7. Mandar mensaje")
-            print("8. Salir")
-            option = input('ingrese la opcion a elegir: ')
-            if(option == 1 ):
-                clear()
-                a = input("primer numero: ")
-                b = input("segundo numero: ")
-                print("\n\n")
-                print(str(a)+"+"+str(b)+" = "+str(proxy.add(a,b)))
-                print("\n\n")
-            elif(option == 2):
-                clear()
-                a = input("primer numero: ")
-                b = input("segundo numero: ")
-                print("\n\n")
-                print(str(a)+"-"+str(b)+" = "+str(proxy.subtract(a,b)))
-                print("\n\n")
-            elif(option == 3 ):
-                clear()
-                a = input("primer numero: ")
-                b = input("segundo numero: ")
-                print("\n\n")
-                print(str(a)+"*"+str(b)+" = "+str(proxy.multiply(a,b)))
-                print("\n\n")
-            elif(option == 4 ):
-                clear()
-                a = input("primer numero: ")
-                b = input("segundo numero: ")
-                print("\n\n")
-                print(str(a)+"/"+str(b)+" = "+str(proxy.divide(a,b)))
-                print("\n\n")
-            elif(option == 5 ):
-                clear()
-                a = input("primer numero: ")
-                b = input("segundo numero: ")
-                multicall = xmlrpclib.MultiCall(proxy)
-                multicall.add(7,3)
-                multicall.subtract(7,3)
-                multicall.multiply(7,3)
-                multicall.divide(7,3)
-                result = multicall()
-                results = tuple(result)
-                print("\n\n")
-                print(str(a)+"+"+str(b)+" = "+str(results[0]))
-                print(str(a)+"-"+str(b)+" = "+str(results[1]))
-                print(str(a)+"*"+str(b)+" = "+str(results[2]))
-                print(str(a)+"/"+str(b)+" = "+str(results[3]))
-                print("\n\n")
-            elif(option == 6 ):
-                clear()
-                print(proxy.serverInfo())
-                print("\n\n")
-            elif(option == 7):
-                print "ingrese el mensaje"
-                a = input()
-                proxy.escribir(a, nombre)
-            elif(option == 8 ):
-                print(proxy.bye(nombre))
-                break
+            userInfo = [nombre,password]
+            ingresado = proxy.ingresar(userInfo)
+            print("\n\n")
+        # print(ingresado)
+    else:
+        a = raw_input("ingrese el mensaje: ")
+        if a == "/salir":
+            print(proxy.bye(nombre))
+            break
+        else:
+            proxy.escribir(a, nombre)
+    
+                
 		
 
 
