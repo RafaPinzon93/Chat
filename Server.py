dc97c61e921182879a00821f984a74e45627435c
noescobar <nelson.holic@gmail.com>
2015-05-19 08:41:13 -0500

    se hicieron mejoras a las intefaces de las clases tanto de cliente como del servidor

--- Server.py
+++ Server.py
@@ -1,56 +1,133 @@
 import xmlrpclib
 import datetime
 from SimpleXMLRPCServer import SimpleXMLRPCServer
+import xmlrpclib
 from threading import Thread
+import thread
 import time
 
 # Nombre = None
 # Passw = None
 lastConn = None
 Contador = -1
-Nombres = {}
+nombres = {}
 
-Usuarios= []
+usuarios = []
 
-class User():
-    def __init__(self, userName, password):
-        self.userName = userName
+
+server = SimpleXMLRPCServer(("localhost", 8000))
+
+
+class User:
+
+    def __init__(self, user_name, password):
+        """
+
+        :param user_name: Este sera el nombre del usuario  al cual se le creara una instancia
+        :param password:
+        :return:
+        """
+        self.user_name = user_name
         self.password = password
+        self.new_messages_bool = False
+        self.chats = {
+            '#public': []
+        }  # Este diccionario cumplira de una forma MUY SIMPLE COMO UNa tabla hash donde se alojaran las
+        # conversaciones del usuario en una especie de pila
+        self.users_objects = {
+
+        }  # La idea es que el usuario pueda tener una relacion directa con los usuario con los cuales este se encuentre
+        #  chateando para de esta forma enviar mensajes en su propia variable chat.
         self.hilo = None
 
+    def get_username(self):
+        return self.user_name
+
+    def send_message(self, message, to_username):
+        """
+        Principalmente se busca, que atraves del diccionario de usuarios con el que un usuario se encuentre chateando
+        este pueda usar su intancia usuario para actualizar sus diccionarios de chats y de esta forma que tenga los
+        mensajes correctamente enviados.
+        Este funcion se diferencia con la funcion de post_message ya que esta ultima es usada para poner un mensaje
+        dentro de los valores del usuario, esta funcion de hecho es usada por send_message para postear un mensaje al
+        usuario en cuestion.
+        :param message: mensaje que va  a ser enviado
+        :param to_username: nombre del usuario al cual se planea enviar el mensaje
+        :return: retorna un valor de verdad si el mensaje es enviado con exito
+        """
+        pass
+
+    def post_message(self, message, from_username):
+        """
+        Esta funcion se utiliza para postear un mensaje proveniente de un usuario al cual podemos saber q
+        :param from_username: un string que me dice cual es el usuario del que proviene el mensaje.
+        :return:
+        """
+        pass
+
+    def get_messages(self, username):
+        """
+
+        :return: diccionario: se devolvera el diccionario con todos lo mensajes de llegada de un usuario y se limpiara
+        """
+        pass
+
+    def get_all_messages(self):
+        """
+
+        :return: diccionario: se devolvera el diccionario con todos lo mensajes de llegada y  de esta forma se limpiaran
+                 los mensajes.
+        """
+
     def __str__(self):
-        return str(self.userName)
+        return str(self.user_name)
 
     def stop(self):
         self.hilo.stop()
 
-    def crearHilo(self):
+    def test_function(self):
+        """
+        Esta funcion se diseno con el fin de probar si se puede ejecutar un funcion de forma remota, siendo
+        independientes del usuario
+        :return: None
+        """
+        print("Esto es una prueba y se esta comprobando que sean diferentes usuarios, el usuario que ejecuto"
+              "la funcion fue = "+self.user_name)
+
+    def crear_hilo(self):
         if not self.hilo:
-            self.hilo = UserProcess(self.userName)
+            self.hilo = UserProcess(self)
             self.hilo.start()
 
+
 class UserProcess(Thread):
-    def __init__(self, nombre):
+
+    def __init__(self, user_object):
         Thread.__init__(self)
         self.runBool = False 
-        self.nombre = nombre
+        self.user = user_object
 
     def run(self):
         self.runBool = True
         while self.runBool:
-            print "hola " + str(self.nombre)
-            time.sleep(1)
+            usernames = nombres.keys()
+            if len(usernames) != self.user.users_objects.keys():
+                for key in nombres:
+                    self.user.users_objects[key] = usuarios[nombres[key]]
+            time.sleep(0.9)
 
     def stop(self):
         self.runBool = False
+
 
 def cont():
     global Contador
     Contador += 1
     return Contador
 
+
 def is_even(n):
-    return n%2 == 0
+    return n % 2 == 0
 
 
 def today():
@@ -58,73 +135,116 @@ def today():
     return xmlrpclib.DateTime(today)
 
 
-def serverInfo():
+def server_info():
     return server.server_address
 
-
-def add(x,y):
-    return x+y
-
-
-def subtract(x, y):
-    return x-y
-
-
-def multiply(x, y):
-    return x*y
-
-
-def divide(x, y):
-    return x/y
     
 def bye(x):
-    Usuarios[Nombres[x]].stop()
+    usuarios[nombres[x]].stop()
     return "Chao %s"%x
-    
+
+
 def ingresar(x):
-    if x[0] in Nombres and Nombres:
-        if x[1] == Usuarios[Nombres[x[0]]].password:
-            server.register_function(add, 'add')
-            server.register_function(subtract, 'subtract')
-            server.register_function(multiply, 'multiply')
-            server.register_function(divide, 'divide')
-            Usuarios[Nombres[x[0]]].crearHilo()
+    """
+
+    :param x:
+    :return:
+    """
+    if x[0] in nombres and nombres:
+        if x[1] == usuarios[nombres[x[0]]].password:
+            usuario = usuarios[nombres[x[0]]]
+            usuario.crear_hilo()
             return True
 
         else:
             return False
     else:
         return False
+
+
 def registro(x):
-    global Nombres
+    """
+
+    :param x:
+    :return:
+    """
+    global nombres
     Nombre = x[0]
     Passw = x[1]
-    Nombres[Nombre] = cont()
+    nombres[Nombre] = cont()
     user = User(Nombre, Passw)
-    Usuarios.append(user)
-    return "Ahora puede ingresar %s al servidor" %x[0]
-    
+    usuarios.append(user)
+    return "Ahora puede ingresar %s al servidor" % x[0]
+
+
+def send_message(from_username, to_username, message):
+    """
+    Este metodo se encargar de crear el protocolo de envio de mensajes con la libreria xmrpc
+    :param from_username:
+    :param to_username:
+    :param message:
+    :return:
+    """
+
+    def funcion():
+        """
+        :return:
+        """
+        usuario_object = None
+        if from_username in nombres.keys():
+            user_position = nombres['from_username']
+            usuario_object = usuarios[user_position]
+
+        if usuario_object:
+            usuario_object.send_message(message=message,to_username=to_username)
+        return "success"
+    thread.start_new_thread(funcion, ())
+    return True
+
+def get_new_message(from_username, other_username):
+    """
+
+    :param from_username:
+    :param other_username:
+    :return:
+    """
+
+
+def get_all_new_messages(from_username):
+    """
+
+    :param from_usernmae:
+    :return:
+    """
+
+def get_all_usernames():
+    return nombres.keys()
+
 
 def hello(x):
-    return "Bienvenido %s\n"%x
+    """
+
+    :param x:
+    :return:
+    """
+    return "Bienvenido %s\n" % x
 
 
-server = SimpleXMLRPCServer(("192.168.100.5", 8000))
-#server = SimpleXMLRPCServer(("localhost", 8000))
+# server = SimpleXMLRPCServer(("192.168.100.5", 8000))
 print "Listening on port 8000..."
 
-server.register_multicall_functions() # Just send a single request for multiple calls
-#server.register_function(add, 'add')
-#server.register_function(subtract, 'subtract')
-#server.register_function(multiply, 'multiply')
-#server.register_function(divide, 'divide')
+server.register_multicall_functions()  # Just send a single request for multiple calls
 server.register_function(bye, 'bye')
 server.register_function(hello, 'hello')
 server.register_function(ingresar, 'ingresar')
 server.register_function(registro, 'registro')
+server.register_function(get_all_new_messages, 'get_all_new_messages')
+server.register_function(get_new_message, 'get_new_message')
+server.register_function(send_message, 'send_message')
 
 
 server.register_function(is_even, "is_even")
 server.register_function(today, "today")
-server.register_function(serverInfo, "serverInfo")
+server.register_function(server_info, "serverInfo")
+
 server.serve_forever()
--- cliente.py
+++ cliente.py
@@ -2,105 +2,152 @@ __author__ = 'noescobar'
 
 import xmlrpclib
 import os
+from threading import Thread
+import time
+import platform
 
-clear = lambda: os.system('clear')
+if platform.system() != 'Windows':
+    clear = lambda: os.system('clear')
+else:
+    clear = lambda: os.system('cls')
 
-proxy = xmlrpclib.ServerProxy("http://192.168.100.5:8000/")
+
+# proxy = xmlrpclib.ServerProxy("http://192.168.100.5:8000/")
+proxy = xmlrpclib.ServerProxy("http://localhost:8000/")
 
 result = proxy.serverInfo()
 
+
+class UserClient:
+    def __init__(self, username, proxy_instance):
+        self.username = username
+        self.current_username_chat = None
+        self.current_chat_messages = None
+        self.general_messages = {}
+        self.new_messages = False
+        self.usernames = []
+        self.hilos = []
+        self.proxy = proxy_instance
+
+    def get_messages(self):
+        """
+
+        :return:
+        """
+        if self.current_username_chat:
+            from_username = self.username
+            other_username = self.current_username_chat
+            self.current_chat_messages = self.proxy.get_new_message(from_username, other_username)
+
+    def get_all_messages(self):
+        self.general_messages = self.proxy.get_all_new_messages(self.username)
+
+    def run_client_process(self):
+        for hilo in self.hilos:
+            hilo.run()
+
+    def stop_client_process(self):
+        for hilo in self.hilos:
+            hilo.stop()
+
+    def get_current_chat(self):
+        return self.current_username_chat
+
+
+class UserProcessNotificator(Thread):
+    def __init__(self, user_client_object):
+        Thread.__init__(self)
+        self.runBool = False
+        self.user_object = user_client_object
+
+    def run(self):
+        self.runBool = True
+        while self.runBool:
+            if self.user_object.current_username_chat:
+                if self.user_object.current_chat_messages:
+                    chat_username = self.user_object.current_username_chat
+                    messages = self.user_object.current_chat_messages
+                    for message in messages:
+                        print(chat_username+" : " + message)
+            if self.user_object.new_messages:
+                print('Alguien te esta escribiendo')
+            time.sleep(1)
+
+    def stop(self):
+        self.runBool = False
+
+class UserProcessInspection(Thread):
+
+    def __init__(self, user_client_object):
+        """
+
+        :param user_client_object:
+        :return:
+        """
+        Thread.__init__(self)
+        self.runBool = False
+        self.user_object = user_client_object
+
+    def run(self):
+        self.runBool = True
+        while self.runBool:
+            self.user_object.usernames = proxy.get_all_usernames()
+            self.user_object.get_messages()
+            self.user_object.get_all_messages()
+            time.sleep(1)
+
+    def stop(self):
+        self.runBool = False
+
+
 print("Bienvenido al cliente\n\n")
-
-#  nombre = raw_input("Ingrese su nombre : ")
-
-#  print(proxy.hello(nombre))
 
 while True:
     print("1. Registrarse")
     print("2. Ingresar")
+    print("3. Salir")
     option = input('ingrese la opcion a elegir: ')
 
-    if(option == 1 ):
+    if option == 1:
         clear()
         nombre = raw_input("Ingrese su nombre : ")
-        while(True):
+        password = None
+        while True:
             password = raw_input("Ingrese su password : ")
             confPass = raw_input("Confirme su password : ")
-            if(password == confPass):
+            if password == confPass:
                 break
-        userInfo = [nombre,password]
+        userInfo = [nombre, password]
         print(str(proxy.registro(userInfo)))
         print("\n\n")
-    elif(option == 2 ):
+    elif option == 2:
         clear()
         nombre = raw_input("Ingrese su nombre : ")
         password = raw_input("Ingrese su password : ")
-        userInfo = [nombre,password]
+        userInfo = [nombre, password]
         ingresado = proxy.ingresar(userInfo)
         print("\n\n")
         print(ingresado)
-        if(ingresado):
-            print("1. sumar dos numeros")
-            print("2. Restar dos numeros")
-            print("3. multiplicar dos numeros")
-            print("4. Dividir dos numeros")
-            print("5. Todas las operaciones anteriores")
-            print("6. Direccion ip y puerto del servidor")
-            print("7. Salir")
-            option = input('ingrese la opcion a elegir: ')
-            if(option == 1 ):
-                clear()
-                a = input("primer numero: ")
-                b = input("segundo numero: ")
-                print("\n\n")
-                print(str(a)+"+"+str(b)+" = "+str(proxy.add(a,b)))
-                print("\n\n")
-            elif(option == 2):
-                clear()
-                a = input("primer numero: ")
-                b = input("segundo numero: ")
-                print("\n\n")
-                print(str(a)+"-"+str(b)+" = "+str(proxy.subtract(a,b)))
-                print("\n\n")
-            elif(option == 3 ):
-                clear()
-                a = input("primer numero: ")
-                b = input("segundo numero: ")
-                print("\n\n")
-                print(str(a)+"*"+str(b)+" = "+str(proxy.multiply(a,b)))
-                print("\n\n")
-            elif(option == 4 ):
-                clear()
-                a = input("primer numero: ")
-                b = input("segundo numero: ")
-                print("\n\n")
-                print(str(a)+"/"+str(b)+" = "+str(proxy.divide(a,b)))
-                print("\n\n")
-            elif(option == 5 ):
-                clear()
-                a = input("primer numero: ")
-                b = input("segundo numero: ")
-                multicall = xmlrpclib.MultiCall(proxy)
-                multicall.add(7,3)
-                multicall.subtract(7,3)
-                multicall.multiply(7,3)
-                multicall.divide(7,3)
-                result = multicall()
-                results = tuple(result)
-                print("\n\n")
-                print(str(a)+"+"+str(b)+" = "+str(results[0]))
-                print(str(a)+"-"+str(b)+" = "+str(results[1]))
-                print(str(a)+"*"+str(b)+" = "+str(results[2]))
-                print(str(a)+"/"+str(b)+" = "+str(results[3]))
-                print("\n\n")
-            elif(option == 6 ):
-                clear()
-                print(proxy.serverInfo())
-                print("\n\n")
-            elif(option == 7 ):
-                print(proxy.bye(nombre))
-                break
-		
-
-
-
+        if ingresado:
+            user_object = UserClient(nombre)
+            while True:
+                print("1. Chat publico")
+                print("2. Chat privado")
+                print("3. Salir")
+                option = input('ingrese la opcion a elegir: ')
+                if option == 1:
+                    clear()
+                    print("\n\n")
+                elif option == 2:
+                    clear()
+                    a = input("primer numero: ")
+                    b = input("segundo numero: ")
+                    print("\n\n")
+                    print(str(a) + "-" + str(b) + " = " + str(proxy.subtract(a, b)))
+                    print("\n\n")
+                elif option == 3:
+                    print(proxy.bye(nombre))
+                    break
+    elif option == 3:
+        print('Adios')
+        break
